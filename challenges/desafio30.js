7d8168d280d845a0b883ff1be374f09ddc5a8279
washington gama <wgamaneto16@gmail.com>
2023-03-15 14:43:05 -0300

    fix

--- challenges/desafio30.js
+++ challenges/desafio30.js
@@ -2,4 +2,4 @@ db.produtos
   .updateOne({ nome: "Big Mac" }, { $unset: { curtidas: "" } });
 
 db.produtos
-  .find({}, { _id: 0, nome: 1, curtidas: 1 },);
+  .find({}, { _id: 0, nome: 1, curtidas: 1 }, );
